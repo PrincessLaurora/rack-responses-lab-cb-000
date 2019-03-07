0d534fda8346c5a0f0b25f292f2f2b2dbc87a585
Laura Felce <crepu83@yahoo.it>
2019-03-07 09:39:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,11 +8,11 @@ class Application
       resp.write "Good Morning"
 
     else
-      resp.write "Good Afternoon"  
+      resp.write "Good Afternoon"
     end
 
-    resp.finish  
-  end  
+    resp.finish
+  end
 
-end  
+end
   
